6faf311779f7b97808407edd13388ad091761376
Ansuman Sahoo <79016311+unsuman@users.noreply.github.com>
2024-10-23 01:31:18 +0530

    support for newsletter subscription add to db (#11)

--- db/contact_store.go
+++ db/contact_store.go
@@ -2,13 +2,16 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/unsuman/webapp/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type ContactStore interface {
 	InsertContact(context.Context, types.ContactMessage) error
+	InsertNewsletterSubscription(context.Context, types.NewsletterSubscription) error
 }
 
 type MongoContactStore struct {
@@ -24,10 +27,30 @@ func NewMongoContactStore(client *mongo.Client, dbname string) *MongoContactStor
 }
 
 func (m *MongoContactStore) InsertContact(ctx context.Context, msg types.ContactMessage) error {
+	msg.Context = "contact"
 	_, err := m.coll.InsertOne(ctx, msg)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (m *MongoContactStore) InsertNewsletterSubscription(ctx context.Context, email types.NewsletterSubscription) error {
+	if isEmailPresent(ctx, m, email.Email) {
+		return fmt.Errorf("email already present")
+	}
+
+	email.Context = "newsletter"
+	_, err := m.coll.InsertOne(ctx, email)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func isEmailPresent(ctx context.Context, m *MongoContactStore, email string) bool {
+	res := m.coll.FindOne(ctx, bson.M{"email": email, "context": "newsletter"})
+	return res.Err() == nil
 }
